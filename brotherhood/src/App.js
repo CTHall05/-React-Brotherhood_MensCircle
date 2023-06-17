ca70436509e948893f2458a97e8ed07752eaff77
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-06-17 16:52:38 +1000

    Initial Setup: First Commit, Taylor

---
+++ brotherhood/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
