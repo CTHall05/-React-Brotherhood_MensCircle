ad2adb873075a82109a4eb9f4da9fc3da4243a19
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-06-25 18:05:59 +1000

    Started Styling the Solutions Componenet

--- brotherhood/src/Components/Facts/Facts.js
+++ brotherhood/src/Components/Facts/Facts.js
@@ -3,50 +3,63 @@ import '../Facts/Facts.css';
 import ManThinking from '../../Images/manthinking.jpg';
 import Divorce from '../../Images/divorce.jpg';
 import Card from './Card/Card';
+import Solution from '../Solution/Solution';
 
 export default function Facts() {
   const [expandedCardIndex, setExpandedCardIndex] = useState(-1);
+  const [showSolution, setShowSolution] = useState(false);
 
   const handleCardClick = (index) => {
     setExpandedCardIndex(index === expandedCardIndex ? -1 : index);
   };
 
+  const handleSolutionClick = () => {
+    setShowSolution(true);
+    document.getElementById('facts-h2').textContent = "The Solution";
+  };
+
+  
+
   return (
     <section id="facts" className="facts-container">
-      <h2 className="facts-container-h2">Facts</h2>
-      <div className="mens-facts-card">
-        <Card
-          title="Male Suicide"
-          image={ManThinking}
-          content={
-            <ul>
-              <li>75% of all suicides are attempted by Men.</li>
-              <li>Suicide is the biggest killer of men under the age of 45.</li>
-              <li>
-                Males aged 85 and over experience the highest age-specific rate
-                of suicide in the west.
-              </li>
-            </ul>
-          }
-          isExpanded={expandedCardIndex === 0}
-          onClick={() => handleCardClick(0)}
-        />
-        <Card
-          title="Relationships"
-          image={Divorce}
-          content={
-            <ul>
-              <li>Research indicates that over 60% of young men are currently single.</li>
-              <li>Women initiate divorce in nearly 70% of cases and so tend to be better prepared to accept it.</li>
-              <li>In the 2022 the divorce rates in the world was at 48%</li>
-              <li>Fathers gain sole custody in approximately 10% of all cases. In around 11% of cases, they are the primary custodial parent. They share custody in about 7% of cases.</li>
-            </ul>
-          }
-          isExpanded={expandedCardIndex === 1}
-          onClick={() => handleCardClick(1)}
-        />
-        {/* Add more Card components here */}
-      </div>
+      <h2 id="facts-h2" className="facts-container-h2">Facts</h2>
+      {!showSolution && (
+        <div className="mens-facts-card">
+          <Card
+            title="Male Suicide"
+            image={ManThinking}
+            content={
+              <ul>
+                <li>80% of all suicides are attempted by Men.</li>
+                <li>Suicide is the biggest killer of men under the age of 45.</li>
+                <li>
+                  Males aged 85 and over experience the highest age-specific rate
+                  of suicide in the west.
+                </li>
+              </ul>
+            }
+            isExpanded={expandedCardIndex === 0}
+            onClick={() => handleCardClick(0)}
+          />
+          <Card
+            title="Relationships"
+            image={Divorce}
+            content={
+              <ul>
+                <li>Research indicates that over 60% of young men are currently single.</li>
+                <li>Women initiate divorce in nearly 70% of cases and so tend to be better prepared to accept it.</li>
+                <li>Nearly half of fathers without any visitation rights still financially support their children</li>
+                <li>Fathers gain sole custody in approximately 10% of all cases. In around 11% of cases, they are the primary custodial parent. They share custody in about 7% of cases.</li>
+              </ul>
+            }
+            isExpanded={expandedCardIndex === 1}
+            onClick={() => handleCardClick(1)}
+          />
+          {/* Add more Card components here */}
+          <button className="button-style" onClick={handleSolutionClick}>The Solution</button>
+        </div>
+      )}
+      {showSolution && <Solution />}
     </section>
   );
 }
--- brotherhood/src/Components/Solution/Solution.js
+++ brotherhood/src/Components/Solution/Solution.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import '../Solution/Solution.css';
+import Friends from '../../Images/men-friends.jpg';
 
-export default function Solution() {
+const Solution = () => {
   return (
-    <h1>YES</h1>
-  )
-}
+    <div className="solution-container">
+      <div className="solution-card-container">
+        <div className="solution-card-image">
+          <img src={Friends} alt="A group of guy friends!"/>
+        </div>
+        <div className="solution-card-content">
+          <h3>Fitness and Competition</h3>
+          <ul>
+            <li>Find a physical activity that you love!</li>
+            <li>Immerce yourself within this community!</li>
+            <li>Embrace healthy competition!</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="solution-card-container">
+        <div className="solution-card-image">
+          <img src={Friends} alt="A group of guy friends!"/>
+        </div>
+        <div className="solution-card-content">
+          <h3>Relationships and Values</h3>
+          <ul>
+            <li>Decide what is important to you in a relationship.</li>
+            <li>What values do you want to live by.</li>
+            <li>What kind of partner do you want.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="solution-card-container">
+        <div className="solution-card-image">
+         <img src={Friends} alt="A group of guy friends!"/>
+        </div>
+        <div className="solution-card-content">
+          <h3>Mastery</h3>
+          <ul>
+            <li>Bullet point 1</li>
+            <li>Bullet point 2</li>
+            <li>Bullet point 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Solution;
