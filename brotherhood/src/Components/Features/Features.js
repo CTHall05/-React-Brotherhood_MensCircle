ddb12e3264b5925dbb5999ab3c3227daaf088f3e
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-06-26 13:02:34 +1000

    Features Component Design Started

--- brotherhood/src/Components/Features/Features.js
+++ brotherhood/src/Components/Features/Features.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ManLookingAtMap from '../../Images/Features-Images/manlookingatmap.jpg';
 import '../Features/Features.css';
 
 export default function Features() {
@@ -7,8 +8,14 @@ export default function Features() {
       <section className='Features-Component-Container'>
         <h2 className='Features-Component-h2'>Features</h2>
         <div className='Features-Search&Connect-div'>
-          <h3>Search and Connect</h3>
-          <p>After you create a profile, you can use our search filter to find likeminded men that have similar interests, values and goals</p>
+          <div className='Features-SearchConnect-Card'>
+            <h3>Search and Connect</h3>
+            <p>Connect with men on the same path</p>
+            <h3>1.</h3>
+            <img className='Features-Component-Img' src={ManLookingAtMap} alt="A man looking at a map!" />
+            <p>After you create a profile, you can use our search filter to find likeminded men that have similar interests, values and goals</p>
+          </div>
+          
         </div>
       </section>
     </>
