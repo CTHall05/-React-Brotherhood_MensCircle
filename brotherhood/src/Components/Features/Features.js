78faa42d2382ece8541853a2c9c3b9aaf3255ec9
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-06-26 20:22:36 +1000

    Test Commit

--- brotherhood/src/Components/Features/Features.js
+++ brotherhood/src/Components/Features/Features.js
@@ -1,21 +1,32 @@
 import React from 'react'
 import ManLookingAtMap from '../../Images/Features-Images/manlookingatmap.jpg';
+import MenOnARoadTrip from '../../Images/Features-Images/menonaroadtrip.jpg';
 import '../Features/Features.css';
 
 export default function Features() {
   return (
     <>
-      <section className='Features-Component-Container'>
+      <section id='features' className='Features-Component-Container'>
         <h2 className='Features-Component-h2'>Features</h2>
-        <div className='Features-Search&Connect-div'>
-          <div className='Features-SearchConnect-Card'>
-            <h3>Search and Connect</h3>
-            <p>Connect with men on the same path</p>
+        <div className='Features-div'>
+          <div className='Features-Component-Card'>
+            <div className='Features-SearchConnect-Title'>
+              <h3>Search and Connect</h3>
+              <p>Connect with men on the same path</p>
+            </div>
             <h3>1.</h3>
             <img className='Features-Component-Img' src={ManLookingAtMap} alt="A man looking at a map!" />
             <p>After you create a profile, you can use our search filter to find likeminded men that have similar interests, values and goals</p>
           </div>
-          
+          <div className='Features-Component-Card'>
+            <div className='Features-SearchConnect-Title'>
+              <h3>Search and Connect</h3>
+              <p>Connect with men on the same path</p>
+            </div>
+            <h3>2.</h3>
+            <img className='Features-Component-Img' src={MenOnARoadTrip} alt="A man looking at a map!" />
+            <p>After you create a profile, you can use our search filter to find likeminded men that have similar interests, values and goals</p>
+          </div>
         </div>
       </section>
     </>
--- brotherhood/src/Components/Header/Header.js
+++ brotherhood/src/Components/Header/Header.js
@@ -1,22 +1,30 @@
-import React from 'react'
+import React from 'react';
 import './Header.css';
 
 export default function Header() {
-
-  const scrollToSection = (event) => {
+  const scrollToSection = (event, sectionId) => {
     event.preventDefault();
-    const factsSection = document.getElementById('facts');
-    factsSection.scrollIntoView({ behavior: 'smooth' });
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
     <header className='header-container'>
       <h1>Brotherhood.</h1>
       <div className='header-nav-anchors'>
-        <a onClick={scrollToSection} href='#facts'>Facts</a>
-        <a href='#about'>About</a>
-        <a href='#contact'>Contact</a>
+        <a onClick={(event) => scrollToSection(event, 'features')} href='#features'>
+          Features
+        </a>
+        <a onClick={(event) => scrollToSection(event, 'facts')} href='#facts'>
+          Facts
+        </a>
+        <a onClick={(event) => scrollToSection(event, 'about')} href='#about'>
+          About
+        </a>
+        <a onClick={(event) => scrollToSection(event, 'contact')} href='#contact'>
+          Contact
+        </a>
       </div>
     </header>
-  )
+  );
 }
